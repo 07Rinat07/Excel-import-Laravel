c6a4d1f5527d540f0c74686674d93a4b3c42e5ed
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-09 19:58:14 +0500

    Create Navigation

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,20 +1,7 @@
 <template>
-    <div class="w-96 mx-auto">
-        <h1>My App</h1>
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-            <Link :href="route('dashboard')" >
-                Dashboard
-            </Link>
-            <Link :href="route('project.index')" >
-                Projects
-            </Link>
-            <Link :href="route('project.import')" >
-                Import
-            </Link>
-            <Link :href="route('task.index')" >
-                Tasks
-            </Link>
-        </div>
+    <div class="w-1/3 mx-auto">
+        <navigation></navigation>
+
         <slot/>
     </div>
 </template>
@@ -22,12 +9,15 @@
 
 <script>
 import { Link} from "@inertiajs/vue3";
+import Navigation from "@/Components/Navigation.vue";
 
 export default {
     name: "MainLayout",
 
     components: {
-        Link
+        Link,
+
+        Navigation
     }
 }
 </script>
